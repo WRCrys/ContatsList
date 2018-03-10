e8860718466136db569c122759e9b88b10c6ae32
Crystyano Almeida <winnerbyjesus@gmail.com>
2018-03-09 22:27:45 -0300

    configuration of the AddContactFragment

--- app/src/main/java/crys/com/contatslist/AddContactFragment.java
+++ app/src/main/java/crys/com/contatslist/AddContactFragment.java
@@ -1,27 +1,226 @@
 package crys.com.contatslist;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import crys.com.contatslist.Utils.ChangePhotoDialog;
+import crys.com.contatslist.Utils.Init;
+import crys.com.contatslist.Utils.UniversalImageLoader;
+import crys.com.contatslist.models.Contact;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by cryst on 08/03/2018.
  */
 
-public class AddContactFragment extends Fragment {
+public class AddContactFragment extends Fragment implements ChangePhotoDialog.OnPhotoReceivedListener{
 
     private static final String TAG = "AddContactFragment";
+
+    //private Contact mContact;
+    private EditText mPhoneNumber, mName, mEmail;
+    private CircleImageView mContactImage;
+    private Spinner mSelectDevice;
+    private Toolbar toolbar;
+    private String mSelectedImagePath;
+    private int mPreviousKeyStroke;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_addcontact, container, false);
         Log.d(TAG, "onCreateView: started");
+        mPhoneNumber = (EditText) view.findViewById(R.id.etContactPhone);
+        mName = (EditText) view.findViewById(R.id.etContactName);
+        mEmail = (EditText) view.findViewById(R.id.etContactEmail);
+        mContactImage = (CircleImageView) view.findViewById(R.id.contactImage);
+        mSelectDevice = (Spinner) view.findViewById(R.id.selectDevice);
+        toolbar = (Toolbar) view.findViewById(R.id.editContactToolbar);
+
+        mSelectedImagePath = null;
+
+        //Load the default images by causing an error
+        UniversalImageLoader.setImage(null, mContactImage, null, "");
+
+        //Set the heading the for the toolbar
+        TextView heading = (TextView)view.findViewById(R.id.textContactToolbar);
+        heading.setText(getString(R.string.add_contact));
+
+        //required for setting up the toolbar
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        setHasOptionsMenu(true);
+
+        //navigation for the backarrow
+        ImageView ivBackArrow = (ImageView) view.findViewById(R.id.ivBackArrow);
+        ivBackArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked back arrow.");
+                //remove previous fragment from the backstack (therefore navigating back)
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+        // save the new contact
+        ImageView ivCheckMark = (ImageView) view.findViewById(R.id.ivCheckMark);
+        ivCheckMark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: saving the contact.");
+                //execute the save method for the database
+            }
+        });
+
+        // initiate the dialog box for choosing an image
+        ImageView ivCamera = (ImageView) view.findViewById(R.id.ivCamera);
+        ivCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /*
+                Make sure all permissions have been verified before opening the dialog
+                 */
+                for(int i = 0; i < Init.PERMISSIONS.length; i++){
+                    String[] permission = {Init.PERMISSIONS[i]};
+                    if(((MainActivity)getActivity()).checkPermission(permission)){
+                        if(i == Init.PERMISSIONS.length - 1){
+
+                            Log.d(TAG, "onClick: opening the 'image selection dialog box'.");
+                            ChangePhotoDialog dialog = new ChangePhotoDialog();
+                            dialog.show(getFragmentManager(), getString(R.string.change_photo_dialog));
+                            dialog.setTargetFragment(AddContactFragment.this, 0);
+                        }
+                    } else {
+                        ((MainActivity)getActivity()).verifyPermissions(permission);
+                    }
+                }
+
+
+            }
+        });
+
+        initOnTextChangeListener();
+
         return view;
+    }
+
+    /**
+     * Initialize the onTextChangeListener for formatting the phone number
+     */
+    private void initOnTextChangeListener(){
+
+        mPhoneNumber.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+
+                mPreviousKeyStroke = i;
+                return false;
+            }
+        });
+
+        mPhoneNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String number = editable.toString();
+                Log.d(TAG, "afterTextChanged: "+number);
+
+                if(number.length() == 3 && mPreviousKeyStroke != KeyEvent.KEYCODE_DEL
+                        && !number.contains("(")){
+                    number = String.format("(%s", editable.toString().substring(0,3));
+                    mPhoneNumber.setText(number);
+                    mPhoneNumber.setSelection(number.length());
+                }
+                else if(number.length() == 5 && mPreviousKeyStroke != KeyEvent.KEYCODE_DEL
+                        && !number.contains(")")){
+                    number = String.format("(%s) %s",
+                            editable.toString().substring(1,4),
+                            editable.toString().substring(4,5));
+                    mPhoneNumber.setText(number);
+                    mPhoneNumber.setSelection(number.length());
+                }
+                else if(number.length() ==10 && mPreviousKeyStroke != KeyEvent.KEYCODE_DEL
+                        && !number.contains("-")){
+                    number = String.format("(%s) %s-%s",
+                            editable.toString().substring(1,4),
+                            editable.toString().substring(6,9),
+                            editable.toString().substring(9,10));
+                    mPhoneNumber.setText(number);
+                    mPhoneNumber.setSelection(number.length());
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.contact_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_delete:
+                Log.d(TAG, "onOptionsItemSelected: deleting contact.");
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * Retrieves the selected image from the bundle (coming from ChangePhotoDialog)
+     * @param bitmap
+     */
+
+    @Override
+    public void getBitmapImage(Bitmap bitmap) {
+        Log.d(TAG, "getBitmapImage: got the bitmap: "+bitmap);
+        //get the bitmap from 'ChangePhotoDialog'
+        if(bitmap != null){
+            //compress the image (if you like)
+            ((MainActivity)getActivity()).compressBitmap(bitmap, 70);
+            mContactImage.setImageBitmap(bitmap);
+        }
+    }
+
+    @Override
+    public void getImagePath(String imagePath) {
+        Log.d(TAG, "getImagePath: got the image path: "+imagePath);
+        if(!imagePath.equals("")){
+            imagePath = imagePath.replace(":/", "://");
+            mSelectedImagePath = imagePath;
+            UniversalImageLoader.setImage(imagePath, mContactImage, null, "");
+        }
     }
 }
